6cdb6eff1c1bb349fa22edc6eec196c94cbc174a
mac <vani4848@yandex.ru>
2025-08-06 14:09:09 +0300

    Добавил дто

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit.booking.dto;
+package ru.practicum.shareit.booking.model;
 
 import lombok.Data;
 import ru.practicum.shareit.booking.Status;
@@ -11,7 +11,7 @@ import java.time.LocalDate;
  * TODO Sprint add-bookings.
  */
 @Data
-public class BookingDto {
+public class Booking {
     private Integer id;
     private LocalDate start;
     private LocalDate end;
