5e81e74f7155f5bc677335a851ba9a40bd36b28b
Podystal-byte <istrizancev@gmail.com>
2025-08-09 14:28:44 +0300

    Добавил mapStruct + добавил ResponseEntity

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDtoMapper.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDtoMapper.java
@@ -3,13 +3,5 @@ package ru.practicum.shareit.booking.dto;
 import ru.practicum.shareit.booking.model.Booking;
 
 public class BookingDtoMapper {
-    public static BookingDto map(Booking booking) {
-        BookingDto bookingDto = new BookingDto();
-        bookingDto.setId(booking.getId());
-        bookingDto.setStart(booking.getStart());
-        bookingDto.setEnd(booking.getEnd());
-        bookingDto.setBooker(booking.getBooker());
-        bookingDto.setStatus(booking.getStatus());
-        return bookingDto;
-    }
+
 }
