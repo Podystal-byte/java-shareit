7796d94c45c80aeb9bd869fcb79b6ef5043aea3a
Podystal-byte <istrizancev@gmail.com>
2025-08-30 18:08:42 +0300

    Тесты

---
+++ server/src/test/java/ru/practicum/shareit/ItemRequestControllerTest.java
@@ -0,0 +1,100 @@
+package ru.practicum.shareit;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.request.ItemRequestController;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.service.ItemRequestService;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ItemRequestController.class)
+class ItemRequestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private ItemRequestService itemRequestService;
+
+    private ItemRequestDto itemRequestDto;
+
+    @BeforeEach
+    void setUp() {
+        itemRequestDto = new ItemRequestDto();
+        itemRequestDto.setDescription("Test description");
+        itemRequestDto.setId(1L);
+    }
+
+    @Test
+    void createRequest_whenRequestIsValid_thenReturnsCreatedRequest() throws Exception {
+        when(itemRequestService.createRequest(anyLong(), any(ItemRequestDto.class)))
+                .thenReturn(itemRequestDto);
+
+        mockMvc.perform(post("/requests")
+                        .header("X-Sharer-User-Id", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(itemRequestDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.description", is("Test description")));
+    }
+
+    @Test
+    void getRequestsByUserId_whenCalled_thenReturnsUserRequests() throws Exception {
+        when(itemRequestService.getRequestsByUserId(anyLong()))
+                .thenReturn(Collections.singletonList(itemRequestDto));
+
+        mockMvc.perform(get("/requests")
+                        .header("X-Sharer-User-Id", 1L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].description", is("Test description")));
+    }
+
+    @Test
+    void getAllRequests_whenCalled_thenReturnsAllRequests() throws Exception {
+        when(itemRequestService.getAllRequests(anyLong(), any(), any()))
+                .thenReturn(List.of(itemRequestDto));
+
+        mockMvc.perform(get("/requests/all")
+                        .header("X-Sharer-User-Id", 1L)
+                        .param("from", "0")
+                        .param("size", "10"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(1)));
+    }
+
+    @Test
+    void getRequestById_whenRequestExists_thenReturnsRequest() throws Exception {
+        when(itemRequestService.getRequestById(anyLong(), anyLong()))
+                .thenReturn(itemRequestDto);
+
+        mockMvc.perform(get("/requests/{requestId}", 1L)
+                        .header("X-Sharer-User-Id", 1L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)));
+    }
+}
